0d347640f7d68e7087577eaee16b5ad68b9f0960
Michael Zopff <michael.zopff@gmail.com>
2019-08-08 12:15:00 -0600

    Basic implementation of GoogleDriveConfig

--- Source/Plugins/GoogleDrivePlugin/GoogleDriveConfig.cs
+++ Source/Plugins/GoogleDrivePlugin/GoogleDriveConfig.cs
@@ -9,36 +9,57 @@ using System.Xml.Linq;
 
 namespace AlephNote.Plugins.GoogleDrive {
     public class GoogleDriveConfig : IRemoteStorageConfiguration {
+        //private const int ID_EXAMPLE
+
+        //public string Example = "this is an example";
         public XElement Serialize(AXMLSerializationSettings opt) {
-            throw new NotImplementedException();
+            var data = new object[] {
+                //new XElement("Example", Example)
+            };
+
+            var r = new XElement("config", data);
+            r.SetAttributeValue("plugin", GoogleDrivePlugin.Name);
+            r.SetAttributeValue("pluginversion", GoogleDrivePlugin.Version.ToString());
+            return r;
         }
 
         public void Deserialize(XElement input, AXMLSerializationSettings opt) {
-            throw new NotImplementedException();
+            if (input.Name.LocalName != "config") throw new Exception("LocalName != 'config'");
+
+            //Example = XHelper.GetChildValue(input, "Example", "this is an example");
         }
 
         public IEnumerable<DynamicSettingValue> ListProperties() {
-            throw new NotImplementedException();
+            //yield return DynamicSettingValue.CreateText(ID_EXAMPLE, "Example", Example);
+            yield break;
         }
 
         public void SetProperty(int id, string value) {
-            throw new NotImplementedException();
+            //if (id == ID_EXAMPLE) Example = value
+            throw new ArgumentException();
         }
 
         public void SetProperty(int id, bool value) {
-            throw new NotImplementedException();
+            throw new ArgumentException();
         }
 
         public void SetProperty(int id, int value) {
-            throw new NotImplementedException();
+            throw new ArgumentException();
         }
 
-        public bool IsEqual(IRemoteStorageConfiguration other) {
-            throw new NotImplementedException();
+        public bool IsEqual(IRemoteStorageConfiguration iother) {
+            var other = iother as GoogleDriveConfig;
+            if (other == null) return false;
+
+            //if (this.Example != other.Example) return false;
+
+            return true;
         }
 
         public IRemoteStorageConfiguration Clone() {
-            throw new NotImplementedException();
+            return new GoogleDriveConfig() {
+                //Example = this.Example;
+            };
         }
 
         public string GetDisplayIdentifier() {
